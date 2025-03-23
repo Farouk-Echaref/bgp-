bd5ec8889fdb6123d0903151d84a3fde6cec1612
Farouk-Echaref <farouk.echcharef20@gmail.com>
2025-03-23 01:06:17 +0000

    fix: script to build docker images needed

--- P1/confs/build_docker_images.sh
+++ P1/confs/build_docker_images.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker build -t alpine-busybox-fechcha ../gns3-images/alpine-busybox/
-docker build -t frr-router-fechcha ../gns3-images/frr-router/
+docker build -t alpine-busybox-fechcha -f ../gns3-images/alpine-busybox/_fech-cha-1_host ../gns3-images/alpine-busybox/
+
+docker build -t frr-router-fechcha -f ../gns3-images/frr-router/_fech-cha-2 ../gns3-images/frr-router/
+
